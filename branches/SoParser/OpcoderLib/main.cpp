d75c345e8bb74590a42205f1a118df1947c2d5a0
timelzayus <timelzayus@4bd0c64e-a048-0410-8b15-4d2e82a74652>
2008-11-14 12:19:32 +0000

    [-] On recommence [+] La ou il faut arriver ;)

--- branches/SoParser/OpcoderLib/main.cpp
+++ branches/SoParser/OpcoderLib/main.cpp
@@ -3,20 +3,63 @@
 
 using namespace SoParse;
 
-class StartRule : Rule
-{
-public:
-	virtual RuleGroup*	getInsideRules()
-	{
-		return &(*(new RuleGroup) << GetChar
-			);
-	}
-private:
-};
-
 int	main()
 {
-	StartRule	sr;
+// 	simpleCString	::=
+// 		'"' #ReadChar('a', 'z') '"'
+// 	;
+	Rule(SimpleCString) =
+			ReadAChar('"')
+		&	ReadCharRange('a', 'z')
+		&	ReadAChar('"')
+	;
 
-	sr.getInsideRules();
+// 	IdentifierStart ::=
+// 		#ReadChar('a', 'z') | #ReadChar('A', 'Z') | #ReadChar('_')
+// 	;
+	Rule(IdentifierStart) =
+			ReadCharRange('a', 'z')
+		|	ReadCharRange('A', 'Z')
+		|	ReadAChar('_')
+	;
+
+// 	Repeaters	::=
+// 		#ReadChar('a')+ #ReadChar('b')? #ReadChar('c') #readChar('d')*
+// 	;
+	Rule(Repeater) =
+			ReadAChar('a') -> Repeat('+')
+		&	ReadAChar('b') -> Repeat('?')
+		&	ReadAChar('c')
+		&	ReadAChar('d') -> Repeat('*')
+	;
+
+// 	IdentifierEnd ::=
+// 		[#ReadChar('a', 'z') | #ReadChar('A', 'Z') | #ReadChar('0', '9') | #ReadChar('_')]*
+// 	;
+	Rule(IdentifierEnd) =
+		(
+				ReadCharRange('a', 'z')
+			|	ReadCharRange('A', 'Z')
+			|	ReadCharRange('0', '9')
+			|	ReadAChar('_')
+		) << Repeat('*')
+	;
+
+// 	Identifier	::=
+// 		IdentifierStart IdentifierEnd
+// 	;
+	Rule(Identifier) =
+		IdentifierStart
+		IdentifierEnd
+	;
+
+// 	GroupWithRepeaters	::=
+// 		#ReadChar('a')+ | #ReadChar('b')? | #ReadChar('c') | #readChar('d')*
+// 	;
+	Rule(GroupWithRepeaters) =
+			ReadAChar('a') -> Repeat('+')
+		|	ReadAChar('b') -> Repeat('?')
+		|	ReadAChar('c')
+		|	ReadAChar('d') -> Repeat('*')
+	;
 }
